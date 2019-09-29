c526636ef1327c91cdb1fd734d023344e3f922f8
haifeng.hu <haifeng.hu@wuage.com>
2019-09-29 18:22:00 +0800

    [说明]1.新增 消息设置 页

--- src/page/settings/message.vue
+++ src/page/settings/message.vue
@@ -1,18 +1,66 @@
 <template>
-   <el-collapse>
-     <el-collapse-item name="msg-1" title="私信设置">
-         <el-switch v-model="stranger" active-text="允许接受陌生人私信" active-color="#13ce66"></el-switch>
-         <el-switch v-model="follower" active-text="允许接受关注者私信" active-color="#13ce66"></el-switch>
-     </el-collapse-item>
-   </el-collapse>
+    <el-scrollbar class="outter_scroll">
+        <el-collapse>
+            <el-collapse-item name="msg-1">
+                <template slot="title">
+                    <h3>私信设置</h3>
+                </template>
+                <div class="side_title">允许谁给我发私信</div>
+                <el-switch v-model="stranger" active-text="允许接受陌生人私信" active-color="#13ce66"></el-switch>
+                <el-switch v-model="follower" active-text="允许接受关注者私信" active-color="#13ce66"></el-switch>
+            </el-collapse-item>
+            <el-collapse-item name="msg-2">
+                <template slot="title">
+                    <h3>邀请/评论消息设置</h3>
+                </template>
+                <div class="side_title">有人对我发出邀请时，我会收到消息通知</div>
+                <el-switch v-model="mention_me" active-text="提及我" active-color="#13ce66"></el-switch>
+                <el-switch v-model="reply_me" active-text="评论或回复我" active-color="#13ce66"></el-switch>
+                <el-switch v-model="invite_me" active-text="邀请我回答问题" active-color="#13ce66"></el-switch>
+            </el-collapse-item>
+            <el-collapse-item name="msg-3">
+                <template slot="title">
+                    <h3>赞同/赞赏消息设置</h3>
+                </template>
+                <div class="side_title">有人对我赞同或赞赏时，我会收到消息通知</div>
+                <el-switch v-model="agree_me" active-text="赞同我的回答" active-color="#13ce66"></el-switch>
+                <el-switch v-model="thanks_me" active-text="感谢我的回答" active-color="#13ce66"></el-switch>
+                <el-switch v-model="appreciate_me" active-text="赞赏了我的回答" active-color="#13ce66"></el-switch>
+            </el-collapse-item>
+            <el-collapse>
+            <el-collapse-item name="msg-4">
+                <template slot="title">
+                    <h3>关注消息设置</h3>
+                </template>
+                <div class="side_title">我的关注有新动态时，我会收到消息通知</div>
+                <el-switch v-model="follow_me" active-text="关注了我" active-color="#13ce66"></el-switch>
+                <el-switch v-model="follow_my_column" active-text="关注了我的专栏" active-color="#13ce66"></el-switch>
+                <el-switch v-model="new_article_in_column" active-text="专栏有新文章" active-color="#13ce66"></el-switch>
+                <el-switch v-model="new_in_followed_answer" active-text="关注的问题有新回答" active-color="#13ce66"></el-switch>
+                <el-switch v-model="certificate_expired" active-text="我的礼券即将过期" active-color="#13ce66"></el-switch>
+            </el-collapse-item>
+            </el-collapse>
+        </el-collapse>
+    </el-scrollbar>
 </template>
 
 <script>
 export default {
     data(){
         return {
-            stranger:true,
+            stranger:false,
             follower:true,
+            mention_me:true,
+            reply_me:true,
+            invite_me:false,
+            agree_me:true,
+            thanks_me:true,
+            appreciate_me:true,
+            follow_me:true,
+            follow_my_column:true,
+            new_article_in_column:true,
+            new_in_followed_answer:true,
+            certificate_expired:false,
         }
     }
 }
@@ -20,17 +68,22 @@ export default {
 
 <style>
 
+    .outter_scroll {
+        height: 500px;
+    }
+
     .el-collapse {
         margin: 0 auto;
-        width: 800px;
+        width: 600px;
     }
 
     .el-switch {
         margin-bottom: 16px;
         display: block;
+        padding-left: 40%;
     }
 
-    .el-collapse-item__wrap {
-        text-align: center;
+    .side_title {
+        margin-bottom: 10px;
     }
 </style>
