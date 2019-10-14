6886baaa08cde0b512169fef9ce99f881c990b8f
haifeng.hu <haifeng.hu@wuage.com>
2019-10-14 19:28:34 +0800

    1.优化样式

--- src/page/order/orderlist.vue
+++ src/page/order/orderlist.vue
@@ -1,45 +1,48 @@
 <template>
   <div>
-    <el-form
-      :model="searchInfo"
-      ref="searchInfo"
-      :inline="true"
-      label-position="right"
-      label-width="100px"
-      size="small"
-    >
-      <el-form-item label="买家手机号">
-        <el-input v-model="searchInfo.buyerPhone"></el-input>
-      </el-form-item>
-      <el-form-item label="买家用户名">
-        <el-input v-model="searchInfo.buyerName"></el-input>
-      </el-form-item>
-      <el-form-item label="订单号">
-        <el-input v-model="searchInfo.orderid"></el-input>
-      </el-form-item>
-      <el-form-item label="创建时间">
-        <el-date-picker
-          v-model="searchInfo.createTime"
-          type="daterange"
-          start-placeholder="起始时间"
-          end-placeholder="截止时间"
-          range-separator="至"
-        ></el-date-picker>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary">查询订单</el-button>
-      </el-form-item>
-    </el-form>
-    <div class="orderTable">
+    <div class="search-mod">
+      <el-form
+        :model="searchInfo"
+        ref="searchInfo"
+        :inline="true"
+        label-position="right"
+        label-width="100px"
+        size="small"
+      >
+        <el-form-item label="买家手机号">
+          <el-input v-model="searchInfo.buyerPhone"></el-input>
+        </el-form-item>
+        <el-form-item label="买家用户名">
+          <el-input v-model="searchInfo.buyerName"></el-input>
+        </el-form-item>
+        <el-form-item label="订单号">
+          <el-input v-model="searchInfo.orderid"></el-input>
+        </el-form-item>
+        <el-form-item label="创建时间">
+          <el-date-picker
+            v-model="searchInfo.createTime"
+            type="daterange"
+            start-placeholder="起始时间"
+            end-placeholder="截止时间"
+            range-separator="至"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item  class="search-button">
+          <el-button type="primary">查询订单</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="order-list-mod">
       <el-table :data="orderInfo" stripe border="true" :fit="false">
         <el-table-column label="订单号" prop="orderid" width="150"></el-table-column>
         <el-table-column label="买家" prop="buyerName" width="150"></el-table-column>
         <el-table-column label="品名" prop="productName" width="150"></el-table-column>
         <el-table-column label="总金额" prop="total" width="100"></el-table-column>
-        <el-table-column label="订单状态" prop="orderStatus" width="120" align="center" ></el-table-column>
-        <el-table-column label="操作" prop="operation" width="150" align="center">
+        <el-table-column label="订单状态" prop="orderStatus" width="120" align="center"></el-table-column>
+        <el-table-column label="操作" prop="operation" width="120" align="center">
           <template>
-            <el-button type="text" >查看</el-button>
+            <el-button type="text">查看</el-button>
             <el-button type="text">编辑</el-button>
           </template>
         </el-table-column>
@@ -124,13 +127,28 @@ export default {
 </script>
 
 <style scoped>
+.order-list-mod,
+.search-mod {
+  width: 850px;
+  margin-left: 20px;
+  padding: 30px;
+  padding-bottom: 10px;
+  box-sizing: border-box;
+  background-color: #fff;
+  margin-bottom: 20px;
+}
+
+.search-button {
+  margin-left: 100px;
+}
+
 .el-table {
   margin-top: 30px;
   width: fit-content;
 }
 
-.orderTable {
-  padding-left: 40px;
+.order-list-mod {
+  padding-left: 20px;
 }
 
 .el-pagination {
